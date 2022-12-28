55f2168434dcc2a90d3eb0e280918ceedd63e135
zhaoyanpeng <zypmain@gmail.com>
2022-12-28 15:05:08 +0800

    feat:更新alfred_test

--- pkg/alfred/alfred_test.go
+++ pkg/alfred/alfred_test.go
@@ -15,7 +15,7 @@ func Test_Convert(t *testing.T) {
 		UnixtimeMillis: 1577836800000,
 	}
 
-	expected := `{"items":[{"uid":"2020-01-01T09:00:00+09:00","title":"2020-01-01T09:00:00+09:00","subtitle":"test: DateTime string (ISO8601, Local)","arg":"2020-01-01T09:00:00+09:00"},{"uid":"2020-01-01T00:00:00","title":"2020-01-01T00:00:00","subtitle":"test: DateTime string (ISO8601, UTC)","arg":"2020-01-01T00:00:00"},{"uid":"1577836800","title":"1577836800","subtitle":"test: Unix Timestamp (s)","arg":"1577836800"},{"uid":"1577836800000","title":"1577836800000","subtitle":"test: Unix Timestamp (ms)","arg":"1577836800000"}]}`
+	expected := `{"items":[{"uid":"","title":"","subtitle":": standardtimestr-short (ms)","arg":""},{"uid":"","title":"","subtitle":": standarddatestr","arg":""},{"uid":"","title":"","subtitle":": standardtimestr-long (mm)","arg":""},{"uid":"2020-01-01T09:00:00+09:00","title":"2020-01-01T09:00:00+09:00","subtitle":"test: DateTime string (ISO8601, Local)","arg":"2020-01-01T09:00:00+09:00"},{"uid":"2020-01-01T00:00:00","title":"2020-01-01T00:00:00","subtitle":"test: DateTime string (ISO8601, UTC)","arg":"2020-01-01T00:00:00"},{"uid":"1577836800","title":"1577836800","subtitle":"test: Unix Timestamp (s)","arg":"1577836800"},{"uid":"1577836800000","title":"1577836800000","subtitle":"test: Unix Timestamp (ms)","arg":"1577836800000"}]}`
 	actual := ConvertToAlfredJSON(value)
 	if actual != expected {
 		t.Errorf("assert failed. expect:%v actual:%v", expected, actual)
